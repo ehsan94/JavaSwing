ab25076ea28bbef1666a9c1574cbbc333d1a2ffc
ehsan94 <arshadehsan94@gmail.com>
2020-02-20 16:03:15 +0100

    Update main.java

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SortTextImpl objImpl = new SortTextImpl();
+		//SortTextImpl objImpl = new SortTextImpl();
 		SortText objSort = new SortTextImpl();
 	
 		
